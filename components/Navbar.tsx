3bd523bc253eb8f2f7fd8c251206dc48532e95ad
unknown <maha.ahmad.k@hotmail.com>
2025-07-09 19:11:09 +0300

    fix the auth file

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -29,8 +29,8 @@ const Navbar = async () => {
                   await signOut({ redirectTo: "/" });
                 }}
               >
-                <button type="submit">
-                  <span className="max-sm:hidden">Logout</span>
+                <button type="submit" className="cursor-pointer">
+                  <span className="max-sm:hidden ">Logout</span>
                   <LogOut className="size-6 sm:hidden text-red-500" />
                 </button>
               </form>
@@ -53,7 +53,7 @@ const Navbar = async () => {
                 await signIn("github");
               }}
             >
-              <button type="submit">Login</button>
+              <button type="submit" className="cursor-pointer">Login</button>
             </form>
           )}
         </div>
